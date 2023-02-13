c7309af296c5403f2b09d1fc6423f22776848bb9
glinka <alexandraglinka@yandex.ru>
2023-02-13 22:28:48 +0300

    Task_2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,13 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //задача 2
+        System.out.println("Задача 2");
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
